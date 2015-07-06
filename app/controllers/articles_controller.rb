43ddbe2e5167d9a98c784dc7d6fccdaf4066b525
arialblack14 <tsirasalexis@yahoo.gr>
2015-07-06 17:40:04 +0300

    Implented form and edit, update and delete actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,9 +21,19 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article.find(params[:id])
-		@article.destroy
+		@article = Article.find(params[:id]).destroy
 
 		redirect_to articles_path
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		redirect_to article_path(@article)
 	end
 end

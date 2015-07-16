5f0c0fbfefb04a4f17a0bb3c946e063cdf6a816f
arialblack14 <tsirasalexis@yahoo.gr>
2015-07-16 17:06:10 +0300

    Sorcery authentication completed

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,9 @@
 class TagsController < ApplicationController
-def index
-	@tags = Tag.all
-end
+	before_filter :require_login, only: [ :destroy ]
+
+	def index
+		@tags = Tag.all
+	end
 
 	def show
 		@tag = Tag.find(params[:id])		
